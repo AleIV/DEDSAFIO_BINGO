ba0303cc07cb093864eee771066bad2c85fc03bb
Héctor Rodríguez <hd.rdz98@gmail.com>
2021-09-23 01:16:45 -0500

    Challenge Medium: Trading with piglins

--- src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
+++ src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
@@ -1,13 +1,16 @@
 package me.aleiv.core.paper.listeners;
 
 import me.aleiv.core.paper.Game;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Piglin;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Strider;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import me.aleiv.core.paper.Core;
+import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
@@ -45,6 +48,7 @@ public class ChallengeMedium implements Listener{
         var game = instance.getGame();
         if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
             return;
+
         Player player = event.getPlayer();
         if (event.getItem().getType() == Material.GOLDEN_APPLE) {
             if (player.getVehicle() != null && player.getVehicle() instanceof Strider) {
@@ -54,6 +58,26 @@ public class ChallengeMedium implements Listener{
                     manager.attempToFind(player, Game.Challenge.STRIDER_GAPPLE, "");
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDropItem(EntityDropItemEvent event) {
+        var game = instance.getGame();
+        if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
+            return;
+
+        if (event.getEntity() instanceof Piglin piglin) {
+            var manager = instance.getBingoManager();
+            Bukkit.getScheduler().runTaskLater(instance, task -> piglin.getNearbyEntities(10, 10, 10).stream()
+                    .filter(e -> e instanceof Player)
+                    .forEach(e -> {
+                        var player = (Player) e;
+                        var table = manager.findTable(player.getUniqueId());
+                        if (table != null) {
+                            manager.attempToFind(player, Game.Challenge.PIGLIN_BARTER, "");
+                        }
+                    }), 1);
         }
     }
 
