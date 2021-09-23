ab0587f172913771e78df798f01c2a52be4a0fa6
Héctor Rodríguez <hd.rdz98@gmail.com>
2021-09-23 01:00:20 -0500

    Challenge Medium: Gapple in strider

--- src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
+++ src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
@@ -1,13 +1,15 @@
 package me.aleiv.core.paper.listeners;
 
 import me.aleiv.core.paper.Game;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Strider;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import me.aleiv.core.paper.Core;
-import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 import java.util.Arrays;
 
@@ -33,6 +35,23 @@ public class ChallengeMedium implements Listener{
                 var table = manager.findTable(player.getUniqueId());
                 if (table != null) {
                     manager.attempToFind(player, Game.Challenge.INVENTORY_STACKS, "");
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onItemConsumed(PlayerItemConsumeEvent event) {
+        var game = instance.getGame();
+        if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
+            return;
+        Player player = event.getPlayer();
+        if (event.getItem().getType() == Material.GOLDEN_APPLE) {
+            if (player.getVehicle() != null && player.getVehicle() instanceof Strider) {
+                var manager = instance.getBingoManager();
+                var table = manager.findTable(player.getUniqueId());
+                if (table != null) {
+                    manager.attempToFind(player, Game.Challenge.STRIDER_GAPPLE, "");
                 }
             }
         }
