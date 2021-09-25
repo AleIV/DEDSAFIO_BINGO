41ba2a4b68369173681fa7c3713ca9fc4de7ebc5
Héctor Rodríguez <hd.rdz98@gmail.com>
2021-09-25 13:16:15 -0500

    Fixed merge conflicts on Challenge

--- src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
+++ src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
@@ -1,15 +1,14 @@
 package me.aleiv.core.paper.listeners;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Piglin;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Strider;
-import org.bukkit.entity.Villager;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
@@ -96,6 +95,29 @@ public class ChallengeMedium implements Listener{
             var table = manager.findTable(player.getUniqueId());
             if (table != null) {
                 manager.attempToFind(player, Game.Challenge.VILLAGER_MAX_TRADE, "");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        var game = instance.getGame();
+        if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
+            return;
+
+        if (event.getEntity() instanceof Player player && event.getDamager() instanceof Projectile projectile) {
+            if (projectile.getShooter() instanceof Player shooter) {
+                if (shooter.equals(player)) return;
+                var manager = instance.getBingoManager();
+                var table = manager.findTable(player.getUniqueId());
+                if (table != null) {
+                    Bukkit.getScheduler().runTaskLater(instance, task -> {
+                        if (table.getPlayerStream().collect(Collectors.toList()).contains(shooter)) {
+                            String data = shooter.getUniqueId() + ";" + player.getUniqueId() + ";" + projectile.getUniqueId();
+                            manager.attempToFind(player, Game.Challenge.CROSSBOW_SHOT, data);
+                        }
+                    }, 1);
+                }
             }
         }
     }
