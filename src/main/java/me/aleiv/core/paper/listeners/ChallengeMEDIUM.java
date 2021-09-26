3e4091e6a1ae5373ec0f86afb8bd1d93ed2f93ff
Héctor Rodríguez <hd.rdz98@gmail.com>
2021-09-25 20:58:01 -0500

    Pre New medium challenges (haybale, lv 6 enc, bed explode)

--- src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
+++ src/main/java/me/aleiv/core/paper/listeners/ChallengeMEDIUM.java
@@ -1,16 +1,24 @@
 package me.aleiv.core.paper.listeners;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.EnchantingTable;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 import io.papermc.paper.event.player.PlayerTradeEvent;
@@ -25,6 +33,11 @@ public class ChallengeMedium implements Listener{
     public ChallengeMedium(Core instance){
         this.instance = instance;
     }
+
+    private final List<Material> beds = List.of(Material.BLACK_BED, Material.BLUE_BED, Material.BROWN_BED,
+            Material.CYAN_BED, Material.GRAY_BED, Material.GREEN_BED, Material.LIGHT_BLUE_BED, Material.LIGHT_GRAY_BED,
+            Material.LIME_BED, Material.MAGENTA_BED, Material.ORANGE_BED, Material.PINK_BED, Material.PURPLE_BED,
+            Material.RED_BED, Material.WHITE_BED, Material.YELLOW_BED);
 
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
@@ -117,6 +130,68 @@ public class ChallengeMedium implements Listener{
                             manager.attempToFind(player, Game.Challenge.CROSSBOW_SHOT, data);
                         }
                     }, 1);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onItemEnchant(EnchantItemEvent event) {
+        var game = instance.getGame();
+        if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
+            return;
+
+        if (event.getEnchantBlock() instanceof EnchantingTable enchantingTable) {
+            Player player = event.getEnchanter();
+            instance.broadcastMessage("Block data: " + enchantingTable.getBlockData());
+            instance.broadcastMessage("Lvl: " + event.getExpLevelCost());
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlaced(BlockPlaceEvent event) {
+        var game = instance.getGame();
+        if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
+            return;
+
+        Block block = event.getBlockPlaced();
+        Player player = event.getPlayer();
+        if (block.getType() == Material.HAY_BLOCK) {
+            Block campfire = block.getWorld().getBlockAt(block.getLocation().add(0, 1, 0));
+            if (campfire.getType() == Material.CAMPFIRE) {
+                var manager = instance.getBingoManager();
+                var table = manager.findTable(player.getUniqueId());
+                if (table != null) {
+                    manager.attempToFind(player, Game.Challenge.CAMPFIRE_HAY_BALE, "");
+                }
+            }
+        } else if (block.getType() == Material.CAMPFIRE) {
+            Block hayBlock = block.getWorld().getBlockAt(block.getLocation().add(0, -1, 0));
+            if (hayBlock.getType() == Material.HAY_BLOCK) {
+                var manager = instance.getBingoManager();
+                var table = manager.findTable(player.getUniqueId());
+                if (table != null) {
+                    manager.attempToFind(player, Game.Challenge.CAMPFIRE_HAY_BALE, "");
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockInteract(PlayerInteractEvent event) {
+        var game = instance.getGame();
+        if (game.getBingoFase() != Game.BingoFase.CHALLENGE && game.getBingoRound() != Game.BingoRound.TWO)
+            return;
+
+        Player player = event.getPlayer();
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block block = event.getClickedBlock();
+            if (block != null && beds.contains(block.getType()) && (player.getWorld().getEnvironment() == World.Environment.NETHER ||
+                    player.getWorld().getEnvironment() == World.Environment.THE_END)) {
+                var manager = instance.getBingoManager();
+                var table = manager.findTable(player.getUniqueId());
+                if (table != null) {
+                    manager.attempToFind(player, Game.Challenge.BED_EXPLODE, "");
                 }
             }
         }
